85134387fc9e927e6c676a1d8bf535a92318c0e2
marccodron <logs@marc-codron.fr>
2021-03-18 14:52:42 +0100

    add first commit

---
+++ sayMyName.js
@@ -0,0 +1,9 @@
+/* 
+Ecrivez un script sayMyName.js qui prend en argument 
+de la ligne de commande une string et affiche un message 
+comme ci-dessous:
+*/
+
+/*
+% node sayMyName.js Sofiane
+My name is Sofiane.
